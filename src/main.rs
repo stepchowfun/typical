6746aa3dadc3f59a6d66ed91832febc4bb412a51
Stephan Boyer <stephan@stephanboyer.com>
2021-03-26 00:27:55 -0700

    Don't increase the stack size
    Even though the parser is based on recursive descent, the recursion depths are quite small in practice.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use clap::{
     },
     Arg, Shell, SubCommand,
 };
-use std::{fs::read_to_string, io::stdout, path::Path, process::exit, thread};
+use std::{fs::read_to_string, io::stdout, path::Path, process::exit};
 
 // The program version
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -35,9 +35,6 @@ const CHECK_SUBCOMMAND: &str = "check";
 const CHECK_SUBCOMMAND_PATH_OPTION: &str = "check-path";
 const SHELL_COMPLETION_SUBCOMMAND: &str = "shell-completion";
 const SHELL_COMPLETION_SUBCOMMAND_SHELL_OPTION: &str = "shell-completion-shell";
-
-// The stack size in bytes.
-const STACK_SIZE: usize = 16 * 1024 * 1024; // 16 mebibytes (MiB)
 
 // Set up the command-line interface.
 fn cli<'a, 'b>() -> App<'a, 'b> {
@@ -213,23 +210,9 @@ fn entry() -> Result<(), Error> {
 
 // Let the fun begin!
 fn main() {
-    // Run everything in a new thread with a big stack.
-    thread::Builder::new()
-        .stack_size(STACK_SIZE)
-        .spawn(|| {
-            // Jump to the entrypoint and report any resulting errors.
-            if let Err(e) = entry() {
-                eprintln!("{}", e);
-                exit(1);
-            }
-        })
-        .unwrap_or_else(|e| {
-            eprintln!("Error spawning thread: {:?}", e);
-            exit(1);
-        })
-        .join()
-        .unwrap_or_else(|e| {
-            eprintln!("Error joining thread: {:?}", e);
-            exit(1);
-        });
+    // Jump to the entrypoint and report any resulting errors.
+    if let Err(e) = entry() {
+        eprintln!("{}", e);
+        exit(1);
+    }
 }
