3111e905a8977cb0e1d931f1780a45f49a68a4c8
Stephan Boyer <stephan@stephanboyer.com>
2021-08-31 01:29:38 -0700

    Implement varint serialization/deserialization

--- integration-tests/rust/src/main.rs
+++ integration-tests/rust/src/main.rs
@@ -1,17 +1,19 @@
 #![deny(clippy::all, clippy::pedantic, warnings)]
 
+#[macro_use]
 mod types;
 
 use {
     std::{
         f64::consts::PI,
         fmt::Debug,
-        io::{self, Error, ErrorKind},
+        io::{self, Error, ErrorKind, Read, Write},
+        mem::size_of,
     },
     types::{Deserialize, Serialize},
 };
 
-fn round_trip<T: Debug + Deserialize + PartialEq + Serialize>(x: &T) -> io::Result<()> {
+fn round_trip_message<T: Debug + Deserialize + PartialEq + Serialize>(x: &T) -> io::Result<()> {
     println!("Value to be serialized: {:?}", x);
 
     let mut buffer = Vec::<u8>::new();
@@ -28,10 +30,34 @@ fn round_trip<T: Debug + Deserialize + PartialEq + Serialize>(x: &T) -> io::Resu
     }
 }
 
+fn round_trip_varint(x: u64) -> io::Result<()> {
+    println!("Value to be serialized: {:?}", x);
+
+    let mut buffer = Vec::<u8>::new();
+    write_varint!(&mut buffer, x, u64)?;
+    println!("Bytes from serialization: {:?}", buffer);
+
+    let y = read_varint!(&mut buffer.as_slice(), u64)?;
+    println!("Value deserialized from those bytes: {:?}", y);
+
+    if y == x {
+        Ok(())
+    } else {
+        Err(Error::new(ErrorKind::Other, "Mismatch!"))
+    }
+}
+
 #[allow(clippy::float_cmp, clippy::shadow_unrelated)]
 fn main() -> io::Result<()> {
-    round_trip::<f64>(&PI)?;
-    round_trip::<bool>(&false)?;
-    round_trip::<bool>(&true)?;
+    round_trip_message::<f64>(&PI)?;
+    println!();
+    round_trip_message::<bool>(&false)?;
+    println!();
+    round_trip_message::<bool>(&true)?;
+    println!();
+    round_trip_varint(u64::MIN)?;
+    println!();
+    round_trip_varint(u64::MAX)?;
+
     Ok(())
 }
