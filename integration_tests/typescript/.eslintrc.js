369a44aa4783ff9a1f0b71ff602b774b885cc50c
Stephan Boyer <stephan@stephanboyer.com>
2021-10-10 02:48:09 -0700

    Set up ESLint for the TypeScript integration test

---
+++ integration_tests/typescript/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'import/extensions': ['error', 'never'],
+    'import/no-extraneous-dependencies': ['error', {}],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
