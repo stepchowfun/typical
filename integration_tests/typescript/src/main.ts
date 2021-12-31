71c586cc779cbd3e32f27b405679a0c95706be1a
Stephan Boyer <stephan@stephanboyer.com>
2021-12-30 22:26:05 -0800

    Implement the serialization code generator for TypeScript

--- integration_tests/typescript/src/main.ts
+++ integration_tests/typescript/src/main.ts
@@ -1,9 +1,19 @@
 import { Comprehensive } from '../generated/types';
 
 const bar: Comprehensive.Bar.BarOut = { field: 'aRequired' };
+const barSize1 = Comprehensive.Bar.Bar.size(bar);
+const arrayBuffer = new ArrayBuffer(barSize1);
+const dataView = new DataView(arrayBuffer);
+const barSize2 = Comprehensive.Bar.Bar.serialize(dataView, 0, bar);
 
 // eslint-disable-next-line no-console
 console.log(bar);
 
 // eslint-disable-next-line no-console
-console.log(Comprehensive.Bar.Bar.size(bar));
+console.log(barSize1);
+
+// eslint-disable-next-line no-console
+console.log(barSize2);
+
+// eslint-disable-next-line no-console
+console.log(arrayBuffer);
