fdf07edbce077743704f7f8eb583bab2d8338659
Stephan Boyer <stephan@stephanboyer.com>
2022-01-06 02:10:24 -0800

    Install v0.7.0 by default

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/typical"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.6.0}"
+  RELEASE="v${VERSION:-0.7.0}"
 
   # Determine which binary to download.
   FILENAME=''
