165bbdd0b02574b42dd66c12692216882f1d7df4
Stephan Boyer <stephan@stephanboyer.com>
2022-01-08 08:55:52 -0800

    Add some helpful messages to the HTML for the web integration tests

--- integration_tests/typescript-web/src/main.ts
+++ integration_tests/typescript-web/src/main.ts
@@ -4,17 +4,34 @@ import runDegenerate from './degenerate';
 import runSchemaEvolution from './schema-evolution';
 import { verifyOmnifile } from './assertions';
 
-console.log('Running circular dependency integration test\u2026\n');
-runCircularDependency();
+const startingParagraph = document.createElement('p');
+startingParagraph.innerHTML = 'Running integration tests\u2026';
+document.body.appendChild(startingParagraph);
 
-console.log('\nRunning comprehensive integration test\u2026\n');
-runComprehensive();
+try {
+  console.log('Running circular dependency integration test\u2026\n');
+  runCircularDependency();
 
-console.log('\nRunning degenerate integration test\u2026\n');
-runDegenerate();
+  console.log('\nRunning comprehensive integration test\u2026\n');
+  runComprehensive();
 
-console.log('\nRunning schema evolution integration test\u2026\n');
-runSchemaEvolution();
+  console.log('\nRunning degenerate integration test\u2026\n');
+  runDegenerate();
 
-console.log('\nVerifying omnifile\u2026\n');
-verifyOmnifile();
+  console.log('\nRunning schema evolution integration test\u2026\n');
+  runSchemaEvolution();
+
+  console.log('\nVerifying omnifile\u2026\n');
+  verifyOmnifile();
+} catch (e) {
+  const failureParagraph = document.createElement('p');
+  failureParagraph.innerHTML =
+    'Integration tests failed. See the console for details.';
+  document.body.appendChild(failureParagraph);
+
+  throw e;
+}
+
+const successParagraph = document.createElement('p');
+successParagraph.innerHTML = 'Integration tests passed.';
+document.body.appendChild(successParagraph);
