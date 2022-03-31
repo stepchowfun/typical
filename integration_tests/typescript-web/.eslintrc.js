335a338196a1e074bba70064dd72710b7077afe3
Stephan Boyer <stephan@stephanboyer.com>
2022-03-30 22:29:28 -0700

    Update `.eslintrc.js`

--- integration_tests/typescript-web/.eslintrc.js
+++ integration_tests/typescript-web/.eslintrc.js
@@ -21,20 +21,23 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    // Modify this rule to accommodate the fact that Typical type names are PascalCase.
+    // Modify this rule to accommodate the fact that React component names are PascalCase and to
+    // skip the rule for members that require quotes (e.g., '&:focus, &:hover' in JSS).
     '@typescript-eslint/naming-convention': [
       'error',
       {
         selector: 'default',
         format: ['camelCase'],
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'allow',
       },
       {
         selector: 'variable',
         format: ['camelCase', 'UPPER_CASE', 'PascalCase'], // We've added 'PascalCase' here.
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'allow',
+      },
+      {
+        // We've added this item.
+        selector: 'memberLike',
+        format: null,
+        modifiers: ['requiresQuotes'],
       },
       {
         selector: 'typeLike',
