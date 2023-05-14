5171dfd87edc86d1b0adb4f5ec1e3cae03682337
Stephan Boyer <stephan@stephanboyer.com>
2023-05-13 17:26:27 -0700

    Install v0.9.3 by default

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/typical"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.9.2}"
+  RELEASE="v${VERSION:-0.9.3}"
 
   # Determine which binary to download.
   FILENAME=''
@@ -28,6 +28,10 @@
   if uname -a | grep -qi 'Darwin.*x86_64'; then
     echo 'x86-64 macOS detected.'
     FILENAME=typical-x86_64-apple-darwin
+  fi
+  if uname -a | grep -qi 'Darwin.*arm64'; then
+    echo 'AArch64 macOS detected.'
+    FILENAME=typical-aarch64-apple-darwin
   fi
 
   # Find a temporary location for the binary.
